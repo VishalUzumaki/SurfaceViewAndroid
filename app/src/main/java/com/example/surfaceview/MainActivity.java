fc4f05cbbd98bbde6e536e818343475840301bb9
VishalUzumaki <vishal41997@gmail.com>
2020-10-12 13:57:52 -0500

    SurfaceView with camera preview

---
+++ app/src/main/java/com/example/surfaceview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.surfaceview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
